e2595c0ad5b419371267f008fb94d3e19caf80aa
Liucccc <974854469@qq.com>
2018-01-30 16:50:02 +0800

    初始化adminLTE

---
+++ src/Presentation/Hi.Frontend/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Hi.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hi.Frontend.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// 用户服务接口
+        /// </summary>
+        private readonly IUserService _userService;
+
+        public HomeController(IUserService userService)
+        {
+            _userService = userService;
+        }
+        // GET: Admin/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            return View();
+        }
+
+        public ActionResult Form()
+        {
+            return View();
+        }
+
+        public ActionResult TableJson()
+        {
+            var post = _userService.FindAll();
+            return Json(post, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
